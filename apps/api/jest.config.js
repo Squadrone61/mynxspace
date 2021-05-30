8c715127643526b6d86def499dd8e03f936242c4
Squadrone61 <safats61@gmail.com>
2021-05-30 22:03:00 +0300

    Revert "nx migrate angular 12"
    This reverts commit 270d7e99

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -2,7 +2,9 @@ module.exports = {
   displayName: 'api',
   preset: '../../jest.preset.js',
   globals: {
-    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
   },
   transform: {
     '^.+\\.[tj]s$': 'ts-jest',
