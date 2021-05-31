a930298a946c6e1155f2f8f4867b72ad14094d90
Squadrone61 <safats61@gmail.com>
2021-06-01 01:37:33 +0300

    cleanup

--- apps/frontendapp/src/app/modules/playground/playground.module.ts
+++ apps/frontendapp/src/app/modules/playground/playground.module.ts
@@ -5,26 +5,9 @@ import { PlaygroundRoutingModule } from './playground-routing.module';
 import { PlaygroundComponent } from './playground/playground.component';
 import { SharedModule } from '../../shared/modules/shared.module';
 import { PatternComponent } from './playground/pattern.component';
-import {
-  ButtonModule,
-  CardModule,
-  DropdownModule,
-  InputModule,
-} from '@libs/ui';
-import { PortalModule } from '@angular/cdk/portal';
 
 @NgModule({
   declarations: [PlaygroundComponent, PatternComponent],
-  imports: [
-    CommonModule,
-    PlaygroundRoutingModule,
-    SharedModule,
-
-    ButtonModule,
-    CardModule,
-    InputModule,
-    DropdownModule,
-    PortalModule,
-  ],
+  imports: [CommonModule, PlaygroundRoutingModule, SharedModule],
 })
 export class PlaygroundModule {}
